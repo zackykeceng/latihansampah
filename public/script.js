3248b2251c4fd0382daf018ecaf35502398e1e96
zacky <149956838+zackykeceng@users.noreply.github.com>
2026-06-16 12:08:34 +0700

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,5 +1,5 @@
 // Konfigurasi Google Sheets (GANTI DENGAN URL DEPLOY APPS SCRIPT ANDA)
-const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbzGdQxw6x9za1H2VmfuEen52neRf-7M7urYjoGKe2TbjAzhwyFtcz0w3zILYNCnU92j/exec';
+const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbw-mfZz75dG8UUWncs8jR6kIVbtuXnb32jDRKD-xNmplWmEbsHby5U4awrQT-CFZGsQ/exec';
 
 // ==================== POLIGON WILAYAH KECAMATAN ====================
 const WILAYAH = {
